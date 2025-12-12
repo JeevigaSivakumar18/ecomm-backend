c7c51c33bcff2a74c786228f6741cca7ee23ada1
Jeeviga Sivakumar <jeevigasivakumar@gmail.com>
2025-12-12 12:15:30 +0530

    Removed all files

--- models/cartModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cartSchema = new mongoose.Schema({
-    ref_id: { type: Number, default: Date.now },
-    prod_id: { type: mongoose.Schema.Types.ObjectId, ref: 'ProductDB', required: true },
-    quantity: { type: Number, required: true, default: 1 }
-});
-
-module.exports = mongoose.model("CartDB", cartSchema);
